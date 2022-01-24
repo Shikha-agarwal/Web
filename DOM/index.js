589f4a4934a9e8d56bd5abfea97715b521106195
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2022-01-24 22:18:22 +0530

    modified

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,17 @@
-document.querySelector("body").onreset();
+var number1= Math.floor(Math.random() * 6)+1;
+var number2= Math.floor(Math.random() * 6)+1;
+
+var randomDice1 = "./images/dice"+ number1 + ".png";
+document.querySelector("img").setAttribute("src",randomDice1);
+
+var randomDice2 = "./images/dice"+ number2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomDice2);
+ 
+if(number1>number2){
+    document.querySelector("h1").innerHTML = "<em><img class='flag' src='./images/flag.png' alt='flag'> Player1 Wins!</em>";
+}
+else if(number2>number1){
+    document.querySelector("h1").innerHTML = "<em>Player2 Wins! <img class='flag' src='./images/flag.png' alt='flag'></em>";
+} else{
+    document.querySelector("h1").innerHTML = "<em>draw!</em>";
+}
